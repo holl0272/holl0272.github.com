f8446d2428cb2dc434d80e4d849b9c1105227e9b
Eric Holland <eric@f-robots.com>
2013-12-29 19:01:19 -0500

    tweek andriod check to script.js and android CSS

--- dev/gwn/js/scripts.js
+++ dev/gwn/js/scripts.js
@@ -4,7 +4,7 @@ var ua = navigator.userAgent.toLowerCase();
 var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
 if(isAndroid) {
   $("#device-stylesheet").attr("href", "css/android.css");
-  alert('v16');
+  alert('v18');
 };
 
 function adjustStyle(width) {
