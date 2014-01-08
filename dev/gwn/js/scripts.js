d725a7f7b1c19f7a8d47895dd771b8f94af95192
Eric Holland <eric@f-robots.com>
2014-01-08 03:11:44 -0500

    made first round of edits

--- dev/gwn/js/scripts.js
+++ dev/gwn/js/scripts.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
 
-var ua = navigator.userAgent.toLowerCase();
-var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
+var device = navigator.userAgent.toLowerCase();
+var isAndroid = device.indexOf("android") > -1;
 if(isAndroid) {
   $("#device-stylesheet").attr("href", "css/android.css");
 };
 
+var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;  // Opera 8.0+
+var isFirefox = typeof InstallTrigger !== 'undefined';  // Firefox 1.0+
+var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;   // At least Safari 3+
+var isChrome = !!window.chrome && !isOpera;     // Chrome 1+
+var isIE = /*@cc_on!@*/false || !!document.documentMode; // At least IE6
+if(isFirefox) {
+  $("#browser-stylesheet").attr("href", "css/firefox.css");
+};
+if(isSafari) {
+  $("#browser-stylesheet").attr("href", "css/safari.css");
+};
+
 function adjustStyle(width) {
   width = parseInt(width);
-    if (width < 824) {
+    if (width < 480) {
         $("#size-stylesheet").attr("href", "css/narrow.css");
     } else {
        $("#size-stylesheet").attr("href", "css/wide.css");
