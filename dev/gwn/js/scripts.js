aa29380fcfc33ba2d7d40a8d003c3234ee5e2d67
Eric Holland <eric@f-robots.com>
2014-02-24 23:14:37 -0500

    add windows.css js call to turnaround

--- dev/gwn/js/scripts.js
+++ dev/gwn/js/scripts.js
@@ -4,7 +4,7 @@ if(window.innerWidth <= 800 && window.innerHeight <= 600) {
 };
 
 $(document).ready(function(){
-console.log('v4');
+console.log('v5');
 var device = navigator.userAgent.toLowerCase();
 var isAndroid = device.indexOf("android") > -1;
 if(isAndroid) {
