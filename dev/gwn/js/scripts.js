c8cd4874644700cba032304976ada31bf11fde02
Eric Holland <eric@f-robots.com>
2014-02-24 23:17:13 -0500

    add windows.css js call to custom lettering

--- dev/gwn/js/scripts.js
+++ dev/gwn/js/scripts.js
@@ -4,7 +4,7 @@ if(window.innerWidth <= 800 && window.innerHeight <= 600) {
 };
 
 $(document).ready(function(){
-console.log('v5');
+console.log('v6');
 var device = navigator.userAgent.toLowerCase();
 var isAndroid = device.indexOf("android") > -1;
 if(isAndroid) {
