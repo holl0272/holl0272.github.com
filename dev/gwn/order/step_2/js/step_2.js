cfaadd67bf5744f2c44e5b020e27f24ebdd38ff4
Eric Holland <eric@f-robots.com>
2014-03-09 18:06:46 -0500

    added file upload form with lightbox preview

--- dev/gwn/order/step_2/js/step_2.js
+++ dev/gwn/order/step_2/js/step_2.js
@@ -89,7 +89,7 @@ $(document).ready(function(){
   });
   */
 
-
+$('input[type=file]').customFile();
 
 
   //NAME
@@ -1021,6 +1021,11 @@ $(document).ready(function(){
     });
     $('#team_name_input').val('').attr('placeholder','EAGLES');
     $('#team_name_info_icon').attr('src', 'images/info.png');
+    $('#js-preview').attr('href', '').attr('title', '');
+    $('#upload_info_icon').attr('src', 'images/info.png');
+    $('.custom-file-upload').remove();
+    $('#logo_upload').html("<div class='custom-file-upload'><input type='file' id='file' name='myfiles[]' multiple /></div>")
+    $('input[type=file]').customFile();
     $('select').change();
     e.preventDefault();
   });
@@ -1061,7 +1066,8 @@ $(document).ready(function(){
     };
   });
 
-//TOGGLES THE LIGHTBOX OVERLAY ELEMENTS
+// TOGGLES THE LIGHTBOX OVERLAY ELEMENTS
+
 $('#front').on('click', function() {
   $('.back_element').hide();
   $('.front_element').show();
@@ -1218,3 +1224,155 @@ function captureValues() {
 
   $("#form_results").show();
 };
+
+//Reference:
+//http://www.onextrapixel.com/2012/12/10/how-to-create-a-custom-file-input-with-jquery-css3-and-php/
+(function($) {
+
+      // Browser supports HTML5 multiple file?
+      var multipleSupport = typeof $('<input/>')[0].multiple !== 'undefined',
+          isIE = /msie/i.test( navigator.userAgent );
+
+      $.fn.customFile = function() {
+
+        return this.each(function() {
+
+          var $file = $(this).addClass('custom-file-upload-hidden'), // the original file input
+              $wrap = $('<div class="file-upload-wrapper">'),
+              $input = $('<input type="text" class="file-upload-input" />'),
+              // Button that will be used in non-IE browsers
+              $button = $('<a type="button" class="upload_btn">Select a File</a>'),
+              // Hack for IE
+              $label = $('<label class="file-upload-button" for="'+ $file[0].id +'">Select a File</label>');
+
+              $button.focus(function() {
+                this.blur();
+              });
+
+          // Hide by shifting to the left so we
+          // can still trigger events
+          $file.css({
+            position: 'absolute',
+            left: '-9999px'
+          });
+
+          $file.change(function(){
+                    var reader = new FileReader();
+        reader.onload = function (e) {
+          $('#js-preview').attr('href', e.target.result);
+        }
+        reader.readAsDataURL(this.files[0]);
+          });
+
+          $wrap.insertAfter( $file )
+            .append( $file, $input, ( isIE ? $label : $button ) );
+
+          // Prevent focus
+          $file.attr('tabIndex', -1);
+          $button.attr('tabIndex', -1);
+
+          $button.click(function () {
+            $file.blur().click(); // Open dialog
+            $button.blur();
+          });
+
+          $file.change(function() {
+
+            var files = [], fileArr, filename;
+
+            // If multiple is supported then extract
+            // all filenames from the file array
+            if ( multipleSupport ) {
+              fileArr = $file[0].files;
+              for ( var i = 0, len = fileArr.length; i < len; i++ ) {
+                files.push( fileArr[i].name );
+              }
+              filename = files.join(', ');
+
+            // If not supported then just take the value
+            // and remove the path to just show the filename
+            } else {
+              filename = $file.val().split('\\').pop();
+            }
+
+            $input.val( filename ) // Set the value
+              .attr('title', filename) // Show filename in title tootlip
+              .blur();
+            $button.blur();
+            uploadInfo();
+          });
+
+          $input.on({
+            blur: function() { $file.trigger('blur'); },
+            keydown: function( e ) {
+              if ( e.which === 13 ) { // Enter
+                if ( !isIE ) { $file.trigger('click'); }
+              } else if ( e.which === 8 || e.which === 46 ) { // Backspace & Del
+                // On some browsers the value is read-only
+                // with this trick we remove the old input and add
+                // a clean clone with all the original events attached
+                $file.replaceWith( $file = $file.clone( true ) );
+                $file.trigger('change');
+                $input.val('');
+              } else if ( e.which === 9 ){ // TAB
+                return;
+              } else { // All other keys
+                return false;
+              }
+            }
+          });
+
+        });
+
+      };
+
+      // Old browser fallback
+      if ( !multipleSupport ) {
+        $( document ).on('change', 'input.customfile', function() {
+
+          var $this = $(this),
+              // Create a unique ID so we
+              // can attach the label to the input
+              uniqId = 'customfile_'+ (new Date()).getTime(),
+              $wrap = $this.parent(),
+
+              // Filter empty input
+              $inputs = $wrap.siblings().find('.file-upload-input')
+                .filter(function(){ return !this.value }),
+
+              $file = $('<input type="file" id="'+ uniqId +'" name="'+ $this.attr('name') +'"/>');
+
+          // 1ms timeout so it runs after all other events
+          // that modify the value have triggered
+          setTimeout(function() {
+            // Add a new input
+            if ( $this.val() ) {
+              // Check for empty fields to prevent
+              // creating new inputs when changing files
+              if ( !$inputs.length ) {
+                $wrap.after( $file );
+                $file.customFile();
+              }
+            // Remove and reorganize inputs
+            } else {
+              $inputs.parent().remove();
+              // Move the input so it's always last on the list
+              $wrap.appendTo( $wrap.parent() );
+              $wrap.find('input').focus();
+            }
+          }, 1);
+          uploadInfo();
+        });
+      }
+}(jQuery));
+
+  //custom logo info toggle
+  function uploadInfo() {
+      // logoLightBox();
+    if($('.file-upload-input').val() != "") {
+      $('#upload_info_icon').attr('src', 'images/check.png');
+    }
+    else {
+      $('#upload_info_icon').attr('src', 'images/info.png');
+    };
+  };
